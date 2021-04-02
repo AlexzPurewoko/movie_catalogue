807d57311d6db4f863c51af8aa9f181baa126e27
AlexzPurewoko <purwoko908@gmail.com>
2021-04-02 22:25:21 +0700

    [module :data] add popular movie mediator with tdd

---
+++ data/src/androidTest/java/id/apwdevs/app/data/mediator/PopularMovieMediatorTest.kt
@@ -0,0 +1,26 @@
+package id.apwdevs.app.data.mediator
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class PopularMovieMediatorTest {
+
+    @Test
+    fun firstLoad_should_load_first_data_only(){}
+
+    @Test
+    fun load_should_be_able_to_saved_on_database() {}
+
+    @Test
+    fun refresh_should_get_data_again_from_api() {}
+
+    @Test
+    fun append_should_be_able_to_load_next_page_when_scrollUp() {}
+
+    @Test
+    fun prepend_should_be_able_to_load_prev_page_when_scrollDown() {}
+}
