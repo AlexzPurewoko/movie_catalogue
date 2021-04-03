0d807b71000f8cc795913c31b97578babe05f644
AlexzPurewoko <purwoko908@gmail.com>
2021-04-03 10:25:17 +0700

    [module :data] add genres to AssertData.kt

--- data/src/androidTest/java/id/apwdevs/app/data/util/AssertData.kt
+++ data/src/androidTest/java/id/apwdevs/app/data/util/AssertData.kt
@@ -1,6 +1,7 @@
 package id.apwdevs.app.data.util
 
 object AssertData {
+    const val GENRE_MOVIES = "genre_movie_OK.json"
     const val TVSHOW_PAGE_1 = "search_tv_query_A_page_1_OK.json"
     const val TVSHOW_PAGE_2 = "search_tv_query_A_page_2_OK.json"
     const val TVSHOW_PAGE_3 = "search_tv_query_A_page_3_OK.json"
