1dd7b8f3a0d9beccf96f33ca92f3b295479749b9
AlexzPurewoko <purwoko908@gmail.com>
2021-04-03 13:57:04 +0700

    [module :data] remove unecessary factories

--- data/src/main/java/id/apwdevs/app/data/di/Database.kt
+++ data/src/main/java/id/apwdevs/app/data/di/Database.kt
@@ -4,21 +4,12 @@ import id.apwdevs.app.data.source.local.entity.RemoteKeysMovie
 import id.apwdevs.app.data.source.local.entity.RemoteKeysTvShow
 import id.apwdevs.app.data.source.local.entity.items.MovieEntity
 import id.apwdevs.app.data.source.local.entity.items.TvEntity
-import id.apwdevs.app.data.source.local.room.AppDatabase
 import id.apwdevs.app.data.source.local.room.dbcase.PagingCaseDb
 import id.apwdevs.app.data.source.local.room.dbcase.PagingMovieCaseDbInteractor
 import id.apwdevs.app.data.source.local.room.dbcase.PagingTvShowCaseDbInteractor
 import org.koin.dsl.module
 
 val dbAccessModule = module {
-    factory { get<AppDatabase>().genreDao() }
-    factory { get<AppDatabase>().movieDao() }
-    factory { get<AppDatabase>().tvShowDao() }
-    factory { get<AppDatabase>().remoteKeysTvShowDao() }
-    factory { get<AppDatabase>().remoteKeysMovieDao() }
-    factory { get<AppDatabase>().favMovieDetail() }
-    factory { get<AppDatabase>().favTvShowDetail() }
-
-    factory<PagingCaseDb<TvEntity, RemoteKeysTvShow>> { PagingTvShowCaseDbInteractor(get(), get()) }
-    factory<PagingCaseDb<MovieEntity, RemoteKeysMovie>> { PagingMovieCaseDbInteractor(get(), get()) }
+    factory<PagingCaseDb<TvEntity, RemoteKeysTvShow>> { PagingTvShowCaseDbInteractor(get()) }
+    factory<PagingCaseDb<MovieEntity, RemoteKeysMovie>> { PagingMovieCaseDbInteractor(get()) }
 }
