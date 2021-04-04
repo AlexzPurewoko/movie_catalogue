34e57a6908b9a39f9dc1e2d7edd9dda0b160f12a
AlexzPurewoko <purwoko908@gmail.com>
2021-04-05 01:54:21 +0700

    [module :core] add backup MovieRepository interface

--- core/src/main/java/id/apwdevs/app/core/domain/repository/MoviesRepository.kt
+++ core/src/main/java/id/apwdevs/app/core/domain/repository/MoviesRepository.kt
@@ -3,11 +3,18 @@ package id.apwdevs.app.core.domain.repository
 import androidx.paging.PagingData
 import id.apwdevs.app.core.domain.model.DetailMovie
 import id.apwdevs.app.core.domain.model.Movies
+import id.apwdevs.app.core.utils.State
 import kotlinx.coroutines.flow.Flow
 
 interface MoviesRepository {
     fun discoverPopularMovies(): Flow<PagingData<Movies>>
     fun searchMovies(query: String, includeAdult: Boolean): Flow<PagingData<Movies>>
     fun getDetailMovie(movieId: Int): Flow<DetailMovie>
+}
+
+interface MovieRepository {
+    fun discoverPopularMovies(): Flow<PagingData<Movies>>
+    fun searchMovies(query: String, includeAdult: Boolean): Flow<PagingData<Movies>>
+    fun getDetailMovie(movieId: Int): Flow<State<DetailMovie>>
 }
 
