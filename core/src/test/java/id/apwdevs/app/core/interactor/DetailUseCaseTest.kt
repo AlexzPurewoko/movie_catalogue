ecfde0df8309e2d5df7c4e9742eb154726561c53
AlexzPurewoko <purwoko908@gmail.com>
2021-04-09 01:05:21 +0700

    [module :core] Adding and finish detail use case

---
+++ core/src/test/java/id/apwdevs/app/core/interactor/DetailUseCaseTest.kt
@@ -0,0 +1,76 @@
+package id.apwdevs.app.core.interactor
+
+import id.apwdevs.app.core.data.FakeDomain
+import id.apwdevs.app.core.domain.repository.MovieRepository
+import id.apwdevs.app.core.domain.repository.TvShowRepository2
+import id.apwdevs.app.core.domain.usecase.DetailInteractor
+import id.apwdevs.app.core.domain.usecase.DetailUseCase
+import id.apwdevs.app.core.utils.State
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class DetailUseCaseTest {
+    @MockK
+    lateinit var movieRepository: MovieRepository
+
+    @MockK
+    lateinit var tvShowRepository: TvShowRepository2
+
+    lateinit var detailUseCase: DetailUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        detailUseCase = DetailInteractor(movieRepository, tvShowRepository)
+    }
+
+    @Test
+    fun `getDetailMovie() must call repository and return right value`() {
+        runBlocking {
+            val movieId = 1
+            val fakeData = FakeDomain.generateDetailMovieDomain(movieId)
+            val expected = flow {
+                emit(State.Success(fakeData))
+            }
+
+            every { movieRepository.getDetailMovie(any()) } returns expected
+
+            val actual = detailUseCase.getDetailMovie(movieId)
+
+            Assert.assertNotNull(actual)
+            Assert.assertEquals(expected, actual)
+
+            verify(exactly = 1) { movieRepository.getDetailMovie(cmpEq(movieId)) }
+
+        }
+    }
+
+    @Test
+    fun `getDetailTvShow() must call repository and return right value`() {
+        runBlocking {
+            val tvId = 1
+            val fakeData = FakeDomain.generateDetailTvDomain(1)
+            val expected = flow {
+                emit(State.Success(fakeData))
+            }
+
+            every { tvShowRepository.getDetailTvShow(any()) } returns expected
+
+            val actual = detailUseCase.getDetailTvShow(tvId)
+
+            Assert.assertNotNull(actual)
+            Assert.assertEquals(expected, actual)
+
+
+            verify(exactly = 1) { tvShowRepository.getDetailTvShow(cmpEq(tvId)) }
+
+        }
+    }
+}
