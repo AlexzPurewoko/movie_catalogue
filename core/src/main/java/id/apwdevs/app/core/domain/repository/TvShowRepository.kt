d89c4ab9aa8e0a7ee59e2e7ac069c42fee95e00b
AlexzPurewoko <purwoko908@gmail.com>
2021-04-08 12:38:48 +0700

    [module :core] Create clone for TvShowRepository.kt

--- core/src/main/java/id/apwdevs/app/core/domain/repository/TvShowRepository.kt
+++ core/src/main/java/id/apwdevs/app/core/domain/repository/TvShowRepository.kt
@@ -3,10 +3,17 @@ package id.apwdevs.app.core.domain.repository
 import androidx.paging.PagingData
 import id.apwdevs.app.core.domain.model.DetailTvShow
 import id.apwdevs.app.core.domain.model.TvShow
+import id.apwdevs.app.core.utils.State
 import kotlinx.coroutines.flow.Flow
 
 interface TvShowRepository {
     fun discoverPopularTvShow(): Flow<PagingData<TvShow>>
     fun searchTvShow(query: String, includeAdult: Boolean): Flow<PagingData<TvShow>>
     fun getDetailTvShow(tvId: Int): Flow<DetailTvShow>
+}
+
+interface TvShowRepository2 {
+    fun discoverPopularTvShow(): Flow<PagingData<TvShow>>
+    fun searchTvShow(query: String, includeAdult: Boolean): Flow<PagingData<TvShow>>
+    fun getDetailTvShow(tvId: Int): Flow<State<DetailTvShow>>
 }
