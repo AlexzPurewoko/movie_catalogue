05dea718dfc32bb7922159c33f5e435fbe983e74
AlexzPurewoko <purwoko908@gmail.com>
2021-04-11 02:32:40 +0700

    [module :core] add test for favorite repository

--- core/src/test/java/id/apwdevs/app/core/repo/FavoriteTvShowRepositoryTest.kt
+++ core/src/test/java/id/apwdevs/app/core/repo/FavoriteTvShowRepositoryTest.kt
@@ -1,5 +1,51 @@
 package id.apwdevs.app.core.repo
 
+import id.apwdevs.app.core.domain.model.DetailTvShow
+import id.apwdevs.app.core.domain.model.TvShow
+import id.apwdevs.app.core.domain.repository.FavoriteRepository
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
 class FavoriteTvShowRepositoryTest {
+
+    private lateinit var favoriteRepo: FavoriteRepository<TvShow, DetailTvShow>
+
+    @Before
+    fun setup() {
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun getAllFavorites_must_return_data_if_any() {
+    }
+
+    @Test
+    fun getFavoriteDetailItem_must_return_data_if_any() {
+    }
+
+    @Test
+    fun checkIsFavorite_should_return_true_when_item_is_occur() {
+    }
+
+    @Test
+    fun checkIsFavorite_should_return_false_if_no_matched_elements() {
+    }
+
+    @Test
+    fun save_should_success_when_saving_data() {
+    }
+
+    @Test
+    fun unFavorite_should_sucess_when_deleting_one_favorited() {
+    }
 
 }
