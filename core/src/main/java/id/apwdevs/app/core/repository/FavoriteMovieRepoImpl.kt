e40eb8ceb4169e5b229cb30d268631ce086c4852
AlexzPurewoko <purwoko908@gmail.com>
2021-04-11 08:20:50 +0700

    [module: core] implement tests and writing code for FavoriteMovieRepoImpl.kt

--- core/src/main/java/id/apwdevs/app/core/repository/FavoriteMovieRepoImpl.kt
+++ core/src/main/java/id/apwdevs/app/core/repository/FavoriteMovieRepoImpl.kt
@@ -1,30 +1,82 @@
 package id.apwdevs.app.core.repository
 
 import id.apwdevs.app.core.domain.model.DetailMovie
+import id.apwdevs.app.core.domain.model.Genre
 import id.apwdevs.app.core.domain.model.Movies
 import id.apwdevs.app.core.domain.repository.FavoriteRepository
+import id.apwdevs.app.core.utils.DomainToEntityMapper
+import id.apwdevs.app.core.utils.EntityToDomainMapper
 import id.apwdevs.app.core.utils.State
+import id.apwdevs.app.data.source.local.entity.Genres
+import id.apwdevs.app.data.source.local.entity.detail.movie.FavDetailMovie
+import id.apwdevs.app.data.source.local.entity.detail.movie.FavDetailMovieEntity
+import id.apwdevs.app.data.source.local.room.dbcase.FavoriteDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class FavoriteMovieRepoImpl : FavoriteRepository<Movies, DetailMovie> {
-    override fun getAllFavorites(): Flow<State<Movies>> {
-        TODO("Not yet implemented")
+class FavoriteMovieRepoImpl(
+        private val favoriteDataSource: FavoriteDataSource<FavDetailMovieEntity, FavDetailMovie>
+) : FavoriteRepository<Movies, DetailMovie> {
+    override fun getAllFavorites(): Flow<State<List<Movies>>> {
+        return flow {
+            emit(State.Loading())
+            try {
+                val data = favoriteDataSource.getAllFavorite()
+                val genres = favoriteDataSource.genres()
+                val mapped = mapData(data, genres)
+
+                emit(State.Success(mapped))
+            } catch (e: Exception) {
+                emit(State.Error(e))
+            }
+        }
     }
 
     override fun getFavoriteDetailItem(id: Int): Flow<State<DetailMovie>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(State.Loading())
+            try {
+                val data = favoriteDataSource.getFavorite(1)
+                val mapper = EntityToDomainMapper.detailMovie(data)
+                emit(State.Success(mapper))
+            } catch (e: Exception) {
+                emit(State.Error(e))
+            }
+        }
     }
 
     override suspend fun checkIsFavorite(id: Int): Boolean {
-        TODO("Not yet implemented")
+        return favoriteDataSource.isFavorite(id)
     }
 
-    override suspend fun save(detailTvShow: DetailMovie) {
-        TODO("Not yet implemented")
+    override suspend fun save(data: DetailMovie) {
+        val mapped = DomainToEntityMapper.favDetailMovie(data)
+        favoriteDataSource.save(mapped)
     }
 
     override suspend fun unFavorite(id: Int) {
-        TODO("Not yet implemented")
+        favoriteDataSource.deleteData(id)
     }
+
+    private suspend fun mapData(entities: List<FavDetailMovieEntity>, listGenres: List<Genres>): List<Movies> {
+        return entities.map { entity ->
+            val idMappers = favoriteDataSource.genreMapper(entity.id)
+            val genres = idMappers.map {
+                val item = listGenres.find { i -> i.id == it }
+                if (item == null) Genre(0, "")
+                else Genre(item.id, item.genreName)
+            }
+
+            Movies(
+                    movieId = entity.id, title = entity.title, overview = entity.overview,
+                    language = entity.originalLanguage, genres = genres, posterPath = entity.posterPath,
+                    backdropPath = entity.backdropPath, releaseDate = entity.releaseDate, voteAverage = entity.voteAverage,
+                    voteCount = entity.voteCount, adult = entity.adult
+            )
+        }
+
+    }
+
+
 }
 
--- core/src/test/java/id/apwdevs/app/core/repo/FavoriteMovieRepositoryTest.kt
+++ core/src/test/java/id/apwdevs/app/core/repo/FavoriteMovieRepositoryTest.kt
@@ -1,9 +1,27 @@
 package id.apwdevs.app.core.repo
 
+import android.content.Context
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import id.apwdevs.app.core.data.FakeDomain
 import id.apwdevs.app.core.domain.model.DetailMovie
 import id.apwdevs.app.core.domain.model.Movies
 import id.apwdevs.app.core.domain.repository.FavoriteRepository
+import id.apwdevs.app.core.repository.FavoriteMovieRepoImpl
+import id.apwdevs.app.core.utils.DomainToEntityMapper
+import id.apwdevs.app.core.utils.State
+import id.apwdevs.app.data.source.local.entity.detail.movie.FavDetailMovie
+import id.apwdevs.app.data.source.local.entity.detail.movie.FavDetailMovieEntity
+import id.apwdevs.app.data.source.local.room.AppDatabase
+import id.apwdevs.app.data.source.local.room.dbcase.FavoriteDataSource
+import id.apwdevs.app.data.source.local.room.dbcase.FavoriteMovieDataSource
+import io.mockk.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,40 +30,163 @@ import org.robolectric.RobolectricTestRunner
 @RunWith(RobolectricTestRunner::class)
 class FavoriteMovieRepositoryTest {
 
+    private val context: Context by lazy {
+        ApplicationProvider.getApplicationContext()
+    }
+
+    private lateinit var appDb: AppDatabase
+
+    private lateinit var favDataSource: FavoriteDataSource<FavDetailMovieEntity, FavDetailMovie>
     private lateinit var favoriteRepo: FavoriteRepository<Movies, DetailMovie>
 
     @Before
     fun setup() {
-
+        appDb = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).allowMainThreadQueries().build()
+        favDataSource = spyk(FavoriteMovieDataSource(appDb))
+        favoriteRepo = FavoriteMovieRepoImpl(favDataSource)
     }
 
     @After
     fun tearDown() {
-
+        appDb.close()
     }
 
     @Test
     fun getAllFavorites_must_return_data_if_any() {
+        runBlocking {
+            // first, save one fav to database
+            val fakeData = FakeDomain.generateDetailMovieDomain(1)
+            val fakeResults = listOf(FakeDomain.generateMovieDomain(1))
+            favoriteRepo.save(fakeData)
+
+            // secondly, tests
+            val favResults = favoriteRepo.getAllFavorites()
+            val observer = mockk<Observer<State<List<Movies>>>>(relaxUnitFun = true)
+
+            var results: List<Movies>? = null
+            favResults.collect {
+                observer.onChanged(it)
+                if (it is State.Success) {
+                    results = it.data
+                }
+            }
+            delay(1000)
+
+            Assert.assertNotNull(results)
+            Assert.assertEquals(fakeResults.size, results?.size)
+            Assert.assertEquals(fakeResults[0].movieId, results?.get(0)?.movieId)
+
+            coVerifyOrder {
+
+                favDataSource.save(DomainToEntityMapper.favDetailMovie(fakeData))
+                observer.onChanged(match {
+                    it is State.Loading
+                })
+
+                favDataSource.getAllFavorite()
+                favDataSource.genres()
+                favDataSource.genreMapper(1)
+
+                observer.onChanged(match {
+                    it is State.Success && it.data == results
+                })
+            }
+            confirmVerified(observer, favDataSource)
+        }
     }
 
     @Test
     fun getFavoriteDetailItem_must_return_data_if_any() {
+        runBlocking {
+            // first, save one fav to database
+            val fakeData = FakeDomain.generateDetailMovieDomain(1)
+            favoriteRepo.save(fakeData)
+
+            // secondly, tests
+            val favResults = favoriteRepo.getFavoriteDetailItem(1)
+            val observer = mockk<Observer<State<DetailMovie>>>(relaxUnitFun = true)
+
+            var results: DetailMovie? = null
+            favResults.collect {
+                observer.onChanged(it)
+                if (it is State.Success) {
+                    results = it.data
+                }
+            }
+            delay(1000)
+
+            Assert.assertNotNull(results)
+            Assert.assertEquals(fakeData.movieId, results?.movieId)
+
+            coVerifyOrder {
+
+                favDataSource.save(DomainToEntityMapper.favDetailMovie(fakeData))
+                observer.onChanged(match {
+                    it is State.Loading
+                })
+
+                favDataSource.getFavorite(1)
+
+                observer.onChanged(match {
+                    it is State.Success && it.data == results
+                })
+            }
+
+            confirmVerified(observer, favDataSource)
+        }
     }
 
     @Test
     fun checkIsFavorite_should_return_true_when_item_is_occur() {
+        runBlocking {
+            val fakeData = FakeDomain.generateDetailMovieDomain(1)
+            favoriteRepo.save(fakeData)
+
+            val isFav = favoriteRepo.checkIsFavorite(1)
+            Assert.assertTrue(isFav)
+            coVerify { favDataSource.isFavorite(1) }
+        }
     }
 
     @Test
     fun checkIsFavorite_should_return_false_if_no_matched_elements() {
+        runBlocking {
+            val isFav = favoriteRepo.checkIsFavorite(1)
+            Assert.assertFalse(isFav)
+            coVerify { favDataSource.isFavorite(1) }
+        }
     }
 
     @Test
     fun save_should_success_when_saving_data() {
+        runBlocking {
+            val fakeData = FakeDomain.generateDetailMovieDomain(1)
+            val fakeMapper = DomainToEntityMapper.favDetailMovie(fakeData)
+            favoriteRepo.save(fakeData)
+
+            val isFav = favoriteRepo.checkIsFavorite(1)
+            Assert.assertTrue(isFav)
+
+            coVerify { favDataSource.save(fakeMapper) }
+        }
     }
 
     @Test
     fun unFavorite_should_sucess_when_deleting_one_favorited() {
+        runBlocking {
+            val fakeData = FakeDomain.generateDetailMovieDomain(1)
+            favoriteRepo.save(fakeData)
+
+            var isFav = favoriteRepo.checkIsFavorite(1)
+            Assert.assertTrue(isFav)
+
+            favoriteRepo.unFavorite(1)
+            isFav = favoriteRepo.checkIsFavorite(1)
+            Assert.assertFalse(isFav)
+            coVerify { favDataSource.deleteData(1) }
+        }
     }
 
 }
+
+object FakeEntity
