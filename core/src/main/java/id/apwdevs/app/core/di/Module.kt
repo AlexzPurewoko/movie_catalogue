b1900e3406de92b422712699edbe950b5ff0e6f6
AlexzPurewoko <purwoko908@gmail.com>
2021-04-11 09:59:25 +0700

    [module: core] move useCase koinmodule to :core

--- core/src/main/java/id/apwdevs/app/core/di/Module.kt
+++ core/src/main/java/id/apwdevs/app/core/di/Module.kt
@@ -5,8 +5,20 @@ import id.apwdevs.app.core.domain.model.DetailTvShow
 import id.apwdevs.app.core.domain.model.Movies
 import id.apwdevs.app.core.domain.model.TvShow
 import id.apwdevs.app.core.domain.repository.*
+import id.apwdevs.app.core.domain.usecase.*
 import id.apwdevs.app.core.repository.*
 import org.koin.dsl.module
+
+val useCaseModule = module {
+    factory<SearchUseCase> { SearchInteractor(get(), get()) }
+    factory<DetailUseCase> { DetailInteractor(get(), get()) }
+
+    factory<FavUseCase> { FavInteractor(get(), get()) }
+    factory<DiscoverPopularUseCase> { DiscoverPopularInteractor(get(), get()) }
+
+    // deprecated! will be deleted
+    factory<FavoriteUseCase> { FavoriteInteractor(get(), get()) }
+}
 
 val repoModule = module {
     factory<MovieRepository> { MovieRepoImpl(get(), get()) }
