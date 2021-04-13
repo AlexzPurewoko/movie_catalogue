f342372c43cb619ea45632341eb7dd83511855e9
AlexzPurewoko <purwoko908@gmail.com>
2021-04-14 01:20:51 +0700

    [test-lib] adding module :androidtestlibs for helper all android instrumentation test

---
+++ test/androidtestlibs/build.gradle.kts
@@ -0,0 +1,49 @@
+import modules.Apps
+import modules.Libs
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-parcelize")
+}
+
+
+android {
+    compileSdkVersion(Apps.compileSdk)
+    buildToolsVersion(Apps.buildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(Apps.minSdk)
+        targetSdkVersion(Apps.targetSdk)
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            debuggable(true)
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Libs.kotlinStd)
+}
