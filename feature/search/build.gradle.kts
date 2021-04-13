d30772e29f26bdf8be5b7f78e0ee1b8e3b83c98e
AlexzPurewoko <purwoko908@gmail.com>
2021-04-14 03:13:45 +0700

    [module :search] cleaning dependencies and change test runner

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         versionCode = Apps.versionCode
         versionName = Apps.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "id.apwdevs.app.test.androdtest.runner.AppTestRunner"
     }
 
     buildFeatures.viewBinding = true
@@ -40,42 +40,9 @@ tasks.withType<KotlinCompile>().configureEach {
 }
 
 dependencies {
-
-//    implementation(project(":res"))
     api(project(":library:res"))
     implementation(project(":app"))
-//    listOf(
-//        Libs.kotlinStd,
-//        Libs.coreKtx,
-//        Libs.constraintLayout,
-//        Libs.circularProgressBar,
-//        Libs.glide,
-//        Libs.paging,
-//        Libs.coroutinesCore,
-//        Libs.coroutinesAndroid,
-//        Libs.corbindCore,
-//        Libs.corbindMaterial,
-//        Libs.corbindAppCompat,
-//
-//        Libs.koinCore,
-//        Libs.koinAndroid,
-//        Libs.koinAndroidViewModel
-//    ).forEach { implementation(it) }
-//    testImplementation(TestLibs.junit)
-//    testImplementation(project(":core").dependencyProject.sourceSets.getByName("test").output)
 
-//    testImplementation(TestLibs.archCoreTesting)
-//    testImplementation(Libs.coroutinesCore)
-//    testImplementation("androidx.arch.core:core-testing:2.1.0")
-//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.3")
-//    testImplementation(TestLibs.paging)
-//    testImplementation("org.robolectric:robolectric:4.5.1")
-//    testImplementation("androidx.test:core:1.3.0")
-//
-//
-//    testImplementation("io.mockk:mockk:1.10.6")
     testImplementation(project(":test:libs"))
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
-    androidTestImplementation("androidx.annotation:annotation:1.1.0")
+    androidTestImplementation(project(":test:androidtestlibs"))
 }
