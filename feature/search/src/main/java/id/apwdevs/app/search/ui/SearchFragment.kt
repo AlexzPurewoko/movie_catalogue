891bff1f1807e79293bbb87eccc7eaf814c63b91
AlexzPurewoko <purwoko908@gmail.com>
2021-04-14 07:08:50 +0700

    [module :search] adding functionality on SearchFragment.kt

--- feature/search/src/main/java/id/apwdevs/app/search/ui/SearchFragment.kt
+++ feature/search/src/main/java/id/apwdevs/app/search/ui/SearchFragment.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import id.apwdevs.app.res.BaseFeatureFragment
 import id.apwdevs.app.res.util.PageType
@@ -17,6 +18,9 @@ import id.apwdevs.app.search.di.searchModule
 import id.apwdevs.app.search.model.SearchItem
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.module.Module
+import ru.ldralighieri.corbind.widget.checkedChanges
+import ru.ldralighieri.corbind.widget.itemSelections
+import ru.ldralighieri.corbind.widget.textChanges
 
 class SearchFragment : BaseFeatureFragment() {
     override val koinModules: List<Module>
@@ -31,30 +35,68 @@ class SearchFragment : BaseFeatureFragment() {
     internal val stateViewModel: StateViewModel by viewModels()
     private var referenceStateFragment: StateDisplayFragment? = null
 
+    private var searchData: SearchVewModel.SearchData? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+        savedInstanceState?.let {
+            searchData = it.getParcelable(SEARCH_DATA_PARCEL)
+        }
         return binding.root
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(SEARCH_DATA_PARCEL, searchData)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initiateView()
+        initAdapter()
 
-
-        searchVewModel.initiateViewInteractions(binding)
-            .observe(viewLifecycleOwner, this::listenInteractions)
+        searchVewModel.initViewInteractions(
+            binding.include.textSearch.textChanges(),
+            binding.isAdult.checkedChanges(),
+            binding.spinner.itemSelections()
+        ).observe(viewLifecycleOwner, this::listenInteractions)
         stateViewModel.callbackFromStateDisplay.observe(
             viewLifecycleOwner,
             ::stateFragmentCallbackObserver
         )
     }
 
-    private fun stateFragmentCallbackObserver(parameters: List<Any>) {
+    private fun initAdapter() {
+        viewPagerAdapter.addLoadStateListener { state ->
 
+            val itemCount = viewPagerAdapter.itemCount
+            when {
+                state.refresh is LoadState.NotLoading && itemCount == 0 ->
+                    callDisplay(StateViewModel.DisplayType.DATA_EMPTY)
+                state.source.refresh is LoadState.Error ->
+                    callDisplay(StateViewModel.DisplayType.ERROR)
+                state.refresh is LoadState.Loading ->
+                    callDisplay(StateViewModel.DisplayType.LOADING)
+                else ->
+                    toggleStateDisplayFragment(false)
+            }
+        }
+    }
+
+    private fun callDisplay(type: StateViewModel.DisplayType) {
+        toggleStateDisplayFragment(true)
+        stateViewModel.callStateFragmentToDisplaySomething(type)
+    }
+
+    private fun stateFragmentCallbackObserver(parameters: List<Any>) {
+        if (parameters[0] == StateViewModel.StateCallType.RETRY)
+            searchData?.let {
+                searchVewModel.search(it)
+            }
     }
 
     private fun initiateView() {
@@ -70,6 +112,18 @@ class SearchFragment : BaseFeatureFragment() {
             referenceStateFragment?.let { show(it) }
         }
 
+        searchData?.let {
+            binding.spinner.setSelection(
+                when (it.pageType) {
+                    PageType.MOVIES -> 0
+                    PageType.TV_SHOW -> 1
+                }
+            );
+
+            binding.isAdult.isChecked = it.includeAdult
+            binding.include.textSearch.setText(it.searchQuery)
+        }
+
         stateViewModel.callStateFragmentToDisplaySomething(StateViewModel.DisplayType.RECOMMENDATION)
     }
 
@@ -80,6 +134,7 @@ class SearchFragment : BaseFeatureFragment() {
                 else hide(it)
             }
         }
+        binding.recyclerView.visibility = if (displayed) View.INVISIBLE else View.VISIBLE
     }
 
     private fun anyClickFromItem(item: SearchItem) {
@@ -96,8 +151,11 @@ class SearchFragment : BaseFeatureFragment() {
             val query = binding.include.textSearch.text.toString()
             if (query.isEmpty()) return
             val includeAdult = binding.isAdult.isChecked
-            searchVewModel.search(query, page, includeAdult)
+
+            val composeData = SearchVewModel.SearchData(query, page, includeAdult)
+            searchVewModel.search(composeData)
                 .observe(viewLifecycleOwner, ::searchResults)
+            searchData = composeData
         }
     }
 
@@ -110,5 +168,7 @@ class SearchFragment : BaseFeatureFragment() {
             id.apwdevs.app.res.R.string.movies,
             id.apwdevs.app.res.R.string.tvshows
         )
+
+        private const val SEARCH_DATA_PARCEL = "SEARCH_DATA"
     }
 }
