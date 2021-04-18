91c5feb77f6a7f04bbe7455b4a946e66e239f536
AlexzPurewoko <purwoko908@gmail.com>
2021-04-18 12:47:40 +0700

    [module :favorite] adding mapper to map movies and tvshows domain into movieshowitem

---
+++ feature/favorite/src/main/java/id/apwdevs/app/favorite/util/Mapper.kt
@@ -0,0 +1,29 @@
+package id.apwdevs.app.favorite.util
+
+import id.apwdevs.app.core.domain.model.Movies
+import id.apwdevs.app.core.domain.model.TvShow
+import id.apwdevs.app.res.data.MovieShowItem
+
+object Mapper {
+    fun mapDomainMovieToMovieShowItem(movie: Movies) =
+        MovieShowItem(
+            movie.movieId,
+            movie.title,
+            movie.overview,
+            movie.backdropPath,
+            movie.genres,
+            movie.voteCount,
+            movie.voteAverage
+        )
+
+    fun mapDomainTvShowToMovieShowItem(tvshow: TvShow) =
+        MovieShowItem(
+            tvshow.tvId,
+            tvshow.name,
+            tvshow.overview,
+            tvshow.backdropPath,
+            tvshow.genres,
+            tvshow.voteCount,
+            tvshow.voteAverage
+        )
+}
