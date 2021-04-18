c22401ff6ff491eda8a3dcd926e2f70780490c9f
AlexzPurewoko <purwoko908@gmail.com>
2021-04-18 12:45:24 +0700

    [module :core] adding support mapper for mapping into other types

--- library/core/src/main/java/id/apwdevs/app/core/utils/State.kt
+++ library/core/src/main/java/id/apwdevs/app/core/utils/State.kt
@@ -2,6 +2,14 @@ package id.apwdevs.app.core.utils
 
 sealed class State<out T>(val data: T?, val error: Throwable?) {
     class Loading() : State<Nothing>(null, null)
-    class Success<T>(data: T) : State<T>(data, null)
-    class Error(error: Throwable) : State<Nothing>(null, error)
+    class Success<T>(data: T?) : State<T>(data, null)
+    class Error(error: Throwable?) : State<Nothing>(null, error)
+
+    fun <To> copyTo(mapCall: (from: T) -> To): State<To> {
+        return when(this) {
+            is Loading -> Loading()
+            is Error -> Error(error)
+            is Success -> Success(data?.let {mapCall(it)})
+        }
+    }
 }
