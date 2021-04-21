350de8f9f45f73c18b584e77e97bf5415688a40b
AlexzPurewoko <purwoko908@gmail.com>
2021-04-21 07:30:06 +0700

    [module :favorite] adding interaction test case for FavoriteFragment

---
+++ feature/favorite/src/androidTest/java/id/apwdevs/app/favorite/FavoriteFragmentTest.kt
@@ -0,0 +1,13 @@
+package id.apwdevs.app.favorite
+
+import id.apwdevs.app.favorite.testcase.FavoriteFragmentInteractionCaseTest
+
+class FavoriteFragmentTest: FavoriteFragmentInteractionCaseTest() {
+    override fun should_display_correct_fragment_when_swiping() {
+        TODO("Not yet implemented")
+    }
+
+    override fun should_display_correct_fragment_when_clicking_tabs() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ feature/favorite/src/androidTest/java/id/apwdevs/app/favorite/testcase/FavoriteFragmentInteractionCaseTest.kt
@@ -0,0 +1,19 @@
+package id.apwdevs.app.favorite.testcase
+
+import id.apwdevs.app.test.androdtest.BaseAndroidTest
+import org.junit.Test
+
+abstract class FavoriteFragmentInteractionCaseTest: BaseAndroidTest() {
+
+    @Test
+    fun should_display_correct_ui(){
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    abstract fun should_display_correct_fragment_when_swiping()
+
+    @Test
+    abstract fun should_display_correct_fragment_when_clicking_tabs()
+
+}
