05a9069d6e4bc8ef880a5f3daca706664b0137b3
AlexzPurewoko <purwoko908@gmail.com>
2021-04-25 14:55:34 +0700

    fix error when building in releases, move to androidtestlibs dependencies into only debug mode.

--- test/androidtestlibs/build.gradle.kts
+++ test/androidtestlibs/build.gradle.kts
@@ -25,14 +25,13 @@ android {
     }
 
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-
+//        getByName("release") {
+//            isMinifyEnabled = true
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
         getByName("debug") {
             debuggable(true)
         }
