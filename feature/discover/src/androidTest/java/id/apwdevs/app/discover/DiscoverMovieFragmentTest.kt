285414824320a0663b8951b7e67285e07ef94f6b
AlexzPurewoko <purwoko908@gmail.com>
2021-05-01 19:52:55 +0700

    adding test for MovieShowFragment.kt

--- feature/discover/src/androidTest/java/id/apwdevs/app/discover/DiscoverMovieFragmentTest.kt
+++ feature/discover/src/androidTest/java/id/apwdevs/app/discover/DiscoverMovieFragmentTest.kt
@@ -1,22 +1,94 @@
 package id.apwdevs.app.discover
 
 import id.apwdevs.app.discover.case.MovieShowFragmentCaseTest
+import id.apwdevs.app.discover.dispatcher.DiscoverMockDispatcher
+import id.apwdevs.app.discover.dispatcher.EmptyMockDispatcher
+import id.apwdevs.app.res.util.PageType
+import id.apwdevs.app.test.androdtest.utils.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.QueueDispatcher
+import org.junit.Assert
 
 class DiscoverMovieFragmentTest: MovieShowFragmentCaseTest() {
+
+    override val pageType: PageType
+        get() = PageType.MOVIES
+
     override fun should_display_data_when_connected_to_internet() {
-        TODO("Not yet implemented")
+        runBlocking {
+            mockWebServer.dispatcher = DiscoverMockDispatcher(context)
+            launchFragment()
+
+            delay(1000)
+            "frameStatus".viewMustBeHidden()
+            "recyclerView".thisViewMustBeDisplayed()
+            "recyclerView".performCheckOnView(
+                RecyclerViewCheckItemAssertion {
+                    val displayedItems = it.childCount
+                    Assert.assertTrue("Items must be displayed on the view", displayedItems > 0)
+                }
+            )
+        }
     }
 
     override fun should_display_empty_data_when_receive_empty_data() {
-        TODO("Not yet implemented")
+        runBlocking {
+            mockWebServer.dispatcher = EmptyMockDispatcher(context)
+            launchFragment()
+
+            delay(1000)
+            "recyclerView".viewMustBeHidden()
+            "frameStatus".thisViewMustBeDisplayed()
+
+            "Empty Data".mustBeDisplayed()
+        }
+
+
     }
 
-    override fun should_display_error_when_not_connected_to_internet() {
-        TODO("Not yet implemented")
+    override fun should_display_error_when_failed_get_data() {
+        runBlocking {
+            mockWebServer.dispatcher = QueueDispatcher()
+            mockWebServer.enqueue(MockResponse().setResponseCode(404))
+            launchFragment()
+
+            delay(1000)
+            "recyclerView".viewMustBeHidden()
+            "frameStatus".thisViewMustBeDisplayed()
+
+            "Error while retrieve data".mustBeDisplayed()
+            "Retry".mustBeDisplayed()
+        }
     }
 
     override fun should_be_able_to_refresh_data_after_clicking_try_again() {
-        TODO("Not yet implemented")
-    }
-}
+        runBlocking {
+            mockWebServer.dispatcher = QueueDispatcher()
+            mockWebServer.enqueue(MockResponse().setResponseCode(404))
+            launchFragment()
 
+            delay(1000)
+            "recyclerView".viewMustBeHidden()
+            "frameStatus".thisViewMustBeDisplayed()
+
+            "Retry".mustBeDisplayed()
+            mockWebServer.dispatcher = DiscoverMockDispatcher(context)
+
+            "Retry".clickThisText()
+
+            delay(1000)
+            "frameStatus".viewMustBeHidden()
+            "recyclerView".thisViewMustBeDisplayed()
+            "recyclerView".performCheckOnView(
+                RecyclerViewCheckItemAssertion {
+                    val displayedItems = it.childCount
+                    Assert.assertTrue("Items must be displayed on the view", displayedItems > 0)
+                }
+            )
+        }
+
+    }
+
+}
--- feature/discover/src/androidTest/java/id/apwdevs/app/discover/case/MovieShowFragmentCaseTest.kt
+++ feature/discover/src/androidTest/java/id/apwdevs/app/discover/case/MovieShowFragmentCaseTest.kt
@@ -1,13 +1,45 @@
 package id.apwdevs.app.discover.case
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withResourceName
+import id.apwdevs.app.discover.di.discoverViewModel
+import id.apwdevs.app.discover.ui.child.MovieShowFragment
+import id.apwdevs.app.res.util.PageType
 import id.apwdevs.app.test.androdtest.BaseAndroidTest
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertNull
 import org.junit.Test
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
 
-abstract class MovieShowFragmentCaseTest: BaseAndroidTest() {
+abstract class MovieShowFragmentCaseTest : BaseAndroidTest() {
+
+    protected abstract val pageType: PageType
+
+
+    override fun setup() {
+        super.setup()
+        loadKoinModules(discoverViewModel)
+    }
+
+    override fun tearDown() {
+        super.tearDown()
+        unloadKoinModules(discoverViewModel)
+    }
 
     @Test
-    fun should_display_minimal_ui() {
+    open fun should_display_minimal_ui() {
+        launchFragment()
 
+        onView(withResourceName("recyclerView")).check { view, noViewFoundException ->
+            assertNotNull(view)
+            assertNull(noViewFoundException)
+        }
+
+        onView(withResourceName("frameStatus")).check { view, noViewFoundException ->
+            assertNotNull(view)
+            assertNull(noViewFoundException)
+        }
     }
 
     @Test
@@ -17,8 +49,11 @@ abstract class MovieShowFragmentCaseTest: BaseAndroidTest() {
     abstract fun should_display_empty_data_when_receive_empty_data()
 
     @Test
-    abstract fun should_display_error_when_not_connected_to_internet()
+    abstract fun should_display_error_when_failed_get_data()
 
     @Test
     abstract fun should_be_able_to_refresh_data_after_clicking_try_again()
+
+    protected fun launchFragment() =
+        launchFragmentInContainer(MovieShowFragment.newInstance(pageType))
 }
