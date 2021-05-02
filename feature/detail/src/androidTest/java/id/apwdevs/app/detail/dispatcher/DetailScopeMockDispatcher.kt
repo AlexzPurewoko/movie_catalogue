4d5bd856e17467707e805d21be06ae5831bddfa0
AlexzPurewoko <purwoko908@gmail.com>
2021-05-02 07:39:05 +0700

    Making it using ScopeDispatcher.kt

--- feature/detail/src/androidTest/java/id/apwdevs/app/detail/dispatcher/DetailScopeMockDispatcher.kt
+++ feature/detail/src/androidTest/java/id/apwdevs/app/detail/dispatcher/DetailScopeMockDispatcher.kt
@@ -1,13 +1,9 @@
 package id.apwdevs.app.detail.dispatcher
 
 import android.content.Context
-import android.util.Log
 import id.apwdevs.app.data.utils.Config
 import id.apwdevs.app.detail.data.AssetData
-import id.apwdevs.app.libs.util.provideResponse
-import okhttp3.mockwebserver.Dispatcher
-import okhttp3.mockwebserver.MockResponse
-import okhttp3.mockwebserver.RecordedRequest
+import id.apwdevs.app.libs.util.ScopeDispatcher
 
 /**
  * Dispatch all the network request needed for scope DetailItemFragment
@@ -15,22 +11,13 @@ import okhttp3.mockwebserver.RecordedRequest
  */
 
 class DetailScopeMockDispatcher(
-    private val context: Context
-) : Dispatcher() {
+    context: Context
+) : ScopeDispatcher(context) {
 
-    private val mappingRequest = mapOf(
+    override val mappingRequest = mapOf(
         "/movie/791373?api_key=${Config.TOKEN}&language=en-US" to AssetData.DETAIL_MOVIE_OK,
         "/tv/88396?api_key=${Config.TOKEN}&language=en-US" to AssetData.DETAIL_TVSHOW_OK,
         "/genre/movie/list?api_key=${Config.TOKEN}&language=en-US" to AssetData.GENRE_MOVIE_OK,
         "/genre/tv/list?api_key=${Config.TOKEN}&language=en-US" to AssetData.GENRE_TVSHOW_OK
     )
-
-    override fun dispatch(request: RecordedRequest): MockResponse {
-        Log.d("MockDispatcher", "Received Request Path: ${request.path}")
-        val req = mappingRequest[request.path]
-        return req?.let {
-            context.provideResponse(it, 200)
-        } ?: context.provideResponse(AssetData.DETAIL_404, 404)
-    }
-
 }
