d745ed2f40b752307d7dbaccc0925ec8677a45a2
AlexzPurewoko <purwoko908@gmail.com>
2021-05-02 09:13:46 +0700

    Completing test case and fix the issues

--- feature/detail/src/androidTest/java/id/apwdevs/app/detail/ui/DetailItemMovieCaseTest.kt
+++ feature/detail/src/androidTest/java/id/apwdevs/app/detail/ui/DetailItemMovieCaseTest.kt
@@ -1,26 +1,30 @@
 package id.apwdevs.app.detail.ui
 
-import androidx.appcompat.widget.AppCompatRatingBar
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.withResourceName
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import id.apwdevs.app.data.source.remote.response.moviedetail.MovieDetailResponse
+import id.apwdevs.app.data.source.remote.response.tvdetail.TvDetailResponse
+import id.apwdevs.app.detail.dispatcher.DetailScopeMockDispatcher
+import id.apwdevs.app.detail.ui.case.DetailItemFragmentCase
 import id.apwdevs.app.detail.ui.helper.DetailMovieHelper
 import id.apwdevs.app.res.util.PageType
-import id.apwdevs.app.test.androdtest.utils.ViewAssertionRunner
-import id.apwdevs.app.test.androdtest.utils.mustBeDisplayed
-import id.apwdevs.app.test.androdtest.utils.mustDisplayedOnView
-import id.apwdevs.app.test.androdtest.utils.swipeUp
+import id.apwdevs.app.test.androdtest.utils.*
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.QueueDispatcher
 import org.junit.Assert
 import org.junit.runner.RunWith
 
-
 @RunWith(AndroidJUnit4::class)
-class DetailItemMovieCaseTest : DetailFragmentBaseCase() {
+class DetailItemMovieCaseTest : DetailItemFragmentCase() {
+
     override val defaultPageType: PageType
         get() = PageType.MOVIES
     override val defaultItemId: Int
         get() = 791373
+
+    override fun setup() {
+        super.setup()
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+    }
 
     override fun should_decide_the_right_ui_helper_from_intent() {
         launchFragment(itemId = 587807).apply {
@@ -30,20 +34,82 @@ class DetailItemMovieCaseTest : DetailFragmentBaseCase() {
 
     override fun should_display_data_when_success() {
         val data = getDefaultData<MovieDetailResponse>()
-        super.should_display_data_when_success()
-        swipeUp()
+        launchFragment()
+
+        "title".thisViewMustBeDisplayed()
+        "poster_image".thisViewMustBeDisplayed()
+        "error_display".viewMustBeHidden()
+        "favorite_fab".thisViewMustBeDisplayed()
+
         with(data) {
             title.mustDisplayedOnView("title")
             overview!!.mustDisplayedOnView("overview")
             genres.forEach { it.name.mustBeDisplayed() }
-            onView(withResourceName("rating_bar")).check(ViewAssertionRunner {
-                Assert.assertNotNull(it)
-                Assert.assertEquals(voteAverage.toFloat(), (it as AppCompatRatingBar).rating)
-            })
+        }
+    }
+
+    override fun should_display_error_when_request_failing() {
+        mockWebServer.dispatcher = QueueDispatcher()
+        mockWebServer.enqueue(MockResponse().setResponseCode(404))
+        launchFragment(itemId = 76767)
+        "error_display".thisViewMustBeDisplayed()
+        "error_text".thisViewMustContainTextExactly("Failed to load resource. Try Again ?")
+        "btn_retry".thisViewMustBeDisplayed()
+    }
+
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    override fun should_reload_data_when_click_retry_button() {
+        val data = when (defaultPageType) {
+            PageType.MOVIES -> getDefaultData<MovieDetailResponse>()
+            PageType.TV_SHOW -> getDefaultData<TvDetailResponse>()
         }
 
+        mockWebServer.dispatcher = QueueDispatcher()
+        mockWebServer.enqueue(MockResponse().setResponseCode(404))
+        launchFragment()
+        "error_display".thisViewMustBeDisplayed()
+
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+        "btn_retry".clickThis()
+        data.let {
+            when (it) {
+                is MovieDetailResponse -> it.title.mustDisplayedOnView("title")
+                is TvDetailResponse -> it.name.mustDisplayedOnView("title")
+            }
+        }
+    }
+
+    override fun should_favorite_button_hide_and_favoritemenu_show_when_scroll_up() {
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+        launchFragment()
+        "favorite_fab".thisViewMustBeDisplayed()
+        "favorite_menu".viewDoesNotExists()
+
+        // scroll up
+        swipeUp()
+
+        "favorite_fab".viewMustBeHidden()
+        "favorite_menu".thisViewMustBeDisplayed()
+    }
+
+    override fun should_change_favorite_icon_after_adding_and_remove_favorite() {
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+        launchFragment()
+        // add to favorite
+        "favorite_fab".thisViewMustBeDisplayed()
+        "favorite_fab".clickThis()
+
+        // check, is the favorite ever shown, or toggle drawable changed ?
+        // for image view test, we need to check tag state on image view.
+        "favorite_fab".mustContainTagsExactly("favorited")
+
+        // cleanup
+        "favorite_fab".clickThis()
+        "favorite_fab".mustContainTagsExactly("unfavorited")
     }
 
 
 }
-
--- feature/detail/src/androidTest/java/id/apwdevs/app/detail/ui/DetailItemTvShowCaseTest.kt
+++ feature/detail/src/androidTest/java/id/apwdevs/app/detail/ui/DetailItemTvShowCaseTest.kt
@@ -1,17 +1,40 @@
 package id.apwdevs.app.detail.ui
 
+import android.util.Log
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
+import androidx.test.espresso.matcher.ViewMatchers.withResourceName
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import id.apwdevs.app.data.source.remote.response.moviedetail.MovieDetailResponse
+import id.apwdevs.app.data.source.remote.response.tvdetail.TvDetailResponse
+import id.apwdevs.app.detail.dispatcher.DetailScopeMockDispatcher
+import id.apwdevs.app.detail.ui.case.DetailItemFragmentCase
+import id.apwdevs.app.detail.ui.helper.DetailMovieHelper
 import id.apwdevs.app.detail.ui.helper.DetailTvShowHelper
 import id.apwdevs.app.res.util.PageType
+import id.apwdevs.app.test.androdtest.utils.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.QueueDispatcher
+import org.hamcrest.core.AllOf.allOf
 import org.junit.Assert
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class DetailItemTvShowCaseTest : DetailFragmentBaseCase() {
+class DetailItemTvShowCaseTest : DetailItemFragmentCase() {
+
     override val defaultPageType: PageType
         get() = PageType.TV_SHOW
     override val defaultItemId: Int
         get() = 88396
+
+    override fun setup() {
+        super.setup()
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+    }
 
     override fun should_decide_the_right_ui_helper_from_intent() {
         launchFragment().apply {
@@ -20,7 +43,84 @@ class DetailItemTvShowCaseTest : DetailFragmentBaseCase() {
     }
 
     override fun should_display_data_when_success() {
-        TODO("Not yet implemented")
+        val data = getDefaultData<TvDetailResponse>()
+        launchFragment()
+
+        "poster_image".thisViewMustBeDisplayed()
+        "error_display".viewMustBeHidden()
+        "favorite_fab".thisViewMustBeDisplayed()
+        onView(withResourceName("poster_image")).perform(ViewActions.swipeUp())
+        Log.e("HELLO", data.toString())
+        with(data) {
+            name.mustBeDisplayed()
+            overview.mustBeDisplayed()
+            genres.forEach { it.name.mustBeDisplayed() }
+        }
+    }
+
+    override fun should_display_error_when_request_failing() {
+        mockWebServer.dispatcher = QueueDispatcher()
+        mockWebServer.enqueue(MockResponse().setResponseCode(404))
+        launchFragment(itemId = 76767)
+        "error_display".thisViewMustBeDisplayed()
+        "error_text".thisViewMustContainTextExactly("Failed to load resource. Try Again ?")
+        "btn_retry".thisViewMustBeDisplayed()
+    }
+
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    override fun should_reload_data_when_click_retry_button() {
+        runBlocking {
+            val data = when (defaultPageType) {
+                PageType.MOVIES -> getDefaultData<MovieDetailResponse>()
+                PageType.TV_SHOW -> getDefaultData<TvDetailResponse>()
+            }
+
+            mockWebServer.dispatcher = QueueDispatcher()
+            mockWebServer.enqueue(MockResponse().setResponseCode(404))
+            launchFragment()
+            "error_display".thisViewMustBeDisplayed()
+            mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+            "btn_retry".clickThis()
+
+            data.let {
+                if(it is TvDetailResponse) {
+                    it.name.mustBeDisplayed()
+                }
+            }
+        }
+
+    }
+
+    override fun should_favorite_button_hide_and_favoritemenu_show_when_scroll_up() {
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+        launchFragment()
+        "favorite_fab".thisViewMustBeDisplayed()
+        "favorite_menu".viewDoesNotExists()
+
+        // scroll up
+        swipeUp()
+
+        "favorite_fab".viewMustBeHidden()
+        "favorite_menu".thisViewMustBeDisplayed()
+    }
+
+    override fun should_change_favorite_icon_after_adding_and_remove_favorite() {
+        mockWebServer.dispatcher = DetailScopeMockDispatcher(context)
+
+        launchFragment()
+        // add to favorite
+        "favorite_fab".thisViewMustBeDisplayed()
+        "favorite_fab".clickThis()
+
+        // check, is the favorite ever shown, or toggle drawable changed ?
+        // for image view test, we need to check tag state on image view.
+        "favorite_fab".mustContainTagsExactly("favorited")
+
+        // cleanup
+        "favorite_fab".clickThis()
+        "favorite_fab".mustContainTagsExactly("unfavorited")
     }
 
 
